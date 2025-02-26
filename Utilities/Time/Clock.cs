f801625003044fd2499af5a5f6adacff0c00dcda
Thomas Ingham <tingham@mac.com>
2025-02-26 01:59:33 -0500

    Add Intervals property and Reset method to Clock class for enhanced time management - Still a work in progress

--- Utilities/Time/Clock.cs
+++ Utilities/Time/Clock.cs
@@ -14,6 +14,8 @@ namespace URBANFORT.Utilities.Time
         // The number of times that the clock can ever tick before it is completed
         public ulong Duration { get; set; }
 
+        public ulong Intervals { get; set; }
+
         private ulong StartTime { get; set; } = 0;
 
         private ulong LastTime { get; set; } = 0;
@@ -23,16 +25,32 @@ namespace URBANFORT.Utilities.Time
         /// </summary>
         public Action<double> Progress { get; set; }
 
+        public Action Clear { get; set; }
+
         public bool IsRunning { get; set; } = false;
         public bool IsCancelled { get; set; } = false;
 
         // The update method must be called in some sort of main loop; we track our own delta time internally
         public void Update () {
-            LastTime = LastTime == 0 ? Godot.Time.GetTicksMsec() - StartTime : Godot.Time.GetTicksMsec() - LastTime;
-            float delta = LastTime / 1000f;
+            ulong deltaTime = LastTime == 0 ? Godot.Time.GetTicksMsec() - StartTime : Godot.Time.GetTicksMsec() - LastTime;
+            float delta = deltaTime / 1000f;
+            LastTime = Godot.Time.GetTicksMsec();
             if (IsRunning && !IsCancelled) {
                 Progress?.Invoke(delta);
+                Intervals++;
+                if (Intervals >= Duration) {
+                    Complete();
+                }
             }
+
+        }
+
+        public void Reset () 
+        {
+            Intervals = 0;
+            IsRunning = false;
+            IsCancelled = false;
+            Clear?.Invoke();
         }
 
         // <region> Delegate Methods
@@ -40,6 +58,7 @@ namespace URBANFORT.Utilities.Time
         public void Start () {
             ClockDelegate?.Started(this);
             StartTime = Godot.Time.GetTicksMsec();
+            LastTime = 0;
             IsRunning = true;
         }
 
